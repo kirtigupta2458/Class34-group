3633a72f96217d296be3aa6fda5a309af631b088
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2021-03-20 07:24:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var myEngine, myWorld;
+var bg;
+var ground;
+var boggie1,boggie2,boggie3;
+var chain1;
+var trainSound 
+var crashSound
+var flag = 0;
+
+function preload(){
+  bg= loadImage("images/bg.jpg");
+  trainSound = loadSound("sound/train.mp3");
+  crashSound = loadSound("sound/train_crossing.mp3");
+}
+function setup() {
+  createCanvas(1200,400);
+  myEngine = Engine.create();
+  myWorld= myEngine.world;
+
+  
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(myEngine);
+
+  
+ 
+  }
+
+ 
